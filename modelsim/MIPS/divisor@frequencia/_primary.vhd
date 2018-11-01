400df2e8f05c89f06c79e93b58af19132fe23b55
Vinicius Furlan <vinicius.cfurlan@gmail.com>
2018-10-31 21:58:41 -0300

    Add files via upload

---
+++ modelsim/MIPS/divisor@frequencia/_primary.vhd
@@ -0,0 +1,8 @@
+library verilog;
+use verilog.vl_types.all;
+entity divisorFrequencia is
+    port(
+        clk             : in     vl_logic;
+        clock           : out    vl_logic
+    );
+end divisorFrequencia;
